c0b466f64786b1029900cb10a19afd03e2e1654d
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-08-27 22:06:09 +0300

    first commit

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+  wrap_parameters format: []
 
   # GET /birds
   def index
@@ -6,12 +7,12 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
-  # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(params.permit(:name, :species))
     render json: bird, status: :created
   end
 
+  
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
